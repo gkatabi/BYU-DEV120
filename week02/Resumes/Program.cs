e9546fef7b5d6daa73f3d73e1dfecc370248b099
Godfrey Katabira <kat21010@byui.edu>
2025-03-13 18:30:20 +0200

    hard

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,61 @@
 using System;
-
-class Program
+namespace Documents;
 {
-    static void Main(string[] args)
+    public class Company()
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        string companyName = ("Name");
+        {
+            string Name = "Brooks Ltd";
+            
+        }
+        public void Description;
+        {
+            Console.WriteLine("Private");
+        }
+    
+        string CompanyName2 = ("Name2");
+        {
+            public void Name2 = "Microsoft company";
+        }
+        
+    
+    public class  Period()
+    {
+        string Years;
+        {
+            public void ._start = "2020";
+            public void ._end = "2028";
+            
+        }
+
+    public class applicant()
+    {
+        public string initials = "Initials";
+        {
+            string SirNameInitials = "  "; 
+            string firstNameInitials = "";
+
+        }
+
+    static void Nain(string[]args)
+    {
+        // Create that what is to be subject in the resume( Applicant ) first
+        Applicant committedApplicant = new Applicant();
+        Applicant committedApplicant = "Bold"
+
+        Company bigCompany = new Company();
+        Company bigCompany = "Brooks Ltd"
+
+        Company2 privateCompany = new company2();
+        Company2 privateCompany = "Microsoft company"
+
+        Job permantJob = new Job();
+        Job permantJob = "Cleaners job"
+
+        Period extendedPeriod =new period();
+        Period extendedPeriod = "Longer" 
+
     }
 }
+         
+         
