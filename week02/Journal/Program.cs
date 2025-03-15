d12a2254242eff9917030bf5d66972c0bc6e68f1
Godfrey Katabira <kat21010@byui.edu>
2025-03-15 16:08:56 +0200

    fractions

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,73 @@
 using System;
-
-class Program
+namespace Documents
 {
-    static void Main(string[] args)
+    class Journal()
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        static void JournaRecordReminder(string[]args)
+        {
+            Console.WriteLine("These are what stop you from writing in your journal daily");
+//      
+            Console.Write  (" I forget", " Iam too bizy", "It's useless", " Idon't know how", " It's npne pf your business");
+            string numbers = "Mynumber.txt";
+            string[] lines = System.IO.File.ReadAllLines(numbers);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(",");
+                string firstnumber = parts[0];
+                string lastnumber = parts[4];
+            
+                Console.WriteLine(line);
+            }
+
+            
+            Console.WriteLine("Write a number related to why you dont write in it");
+            string Reasons = Console.ReadLine();
+            // Call for a while loop and a break point in case of irritations
+        }
+
+// create the function you will always use to invoke current time
+
+        public static void Time(string[])
+        {
+            import Time
+            time = tim.now();
+            return time; 
+            
+        }        
+
+
+
+        static int  SixHourReminder1(num)
+        {
+            startingTime();
+            firstHour + 5;
+            {
+                
+            }
+        https://github.com/byui-cse/cse210-ww-student-template    
+     
+        }
+
+        int SixHourlyReminder2(int)
+        {
+            Time();
+        
+            {
+                Console.WriteLine("Bip!!!! Bip!!! It's time for journaling");
+            }
+
+
+        }
+
+        public int SixHourlyReminder3(int)
+        {
+            Time();
+            int currentTime = new int Time();
+            // if condition which will awaken the alarm
+            {
+                Console.WriteLine("Bip!!!!1Bip!!! It's time for journaling");
+            }
+        }
     }
 }
