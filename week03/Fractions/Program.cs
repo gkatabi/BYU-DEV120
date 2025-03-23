b5f0d1468fc44af932de2b3f88a17947fafe7d1c
Godfrey Katabira <kat21010@byui.edu>
2025-03-23 07:39:07 +0200

    geters

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,28 +1,63 @@
 using System;
-namespace Program;
-class Fraction
+namespace Documents
 {
-// Attributes space....... still obliged to differenciate between them and objects
-    private int top;
-    private int bottom;
 
-// The constructor without parameters
-    private Fraction ()
+
+    public class Fraction
     {
-        top = 1;
-        bottom = 1;
-        // A constructor of a value 5 at top but denominator is  none
-        top = 5;
-        bottom  = 
-        // A constructor with a top and a bottom
-        top =4 ;
-        bottom = 7;
-    }
+        //                                Attributes space....... still obliged to differenciate between them and objects
+        public static int Top(int n)
+        {
+            return n;
+
+        }
     
-    static void Main(string[]args )
+
+        public static string Middle(string c)
+        {
+            c = "/";
+            return  c;
+        }
+        public static int Bottom(int b)
+        {
+            
+            return b ; 
+        }
+    
+    public class Divide
+    {
+            public static int n { get; private set; }
+            public static int b { get; private set; }
+
+
+            private static object m(float m )
+        {
+        
+            return m;
+
+        }
+    }
+
+        private static int Bottom()
+        {
+            List <int> b =  new List<int> ();
+            {
+                Console.WriteLine("Enter a whole number");
+                int number = Convert .ToInt32(Console.ReadLine());
+                r4rrreturn number; 
+            }
+        }
+
+
+        private static int Top()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Main(string[]args )
 
     {
-        Fraction FractionHolder = new Fraction();
-        Console.WriteLine(FractionHolder.top +"/"+ FractionHolder.bottom);//"Hello World! This is the Fractions Project.");
+        Fraction Divide  = new Fraction();
+        Console.WriteLine(FractionHolder.n +"/"+ FractionHolder.bottom);//"Hello World! This is the Fractions Project.");
     }
 }
