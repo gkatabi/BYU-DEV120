d9840fc2d4aec1aecfe0fa26969e0a797cd4aa83
Godfrey Katabira <kat21010@byui.edu>
2025-03-23 16:43:53 +0200

    new

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -38,7 +38,7 @@ using System.Collections.Generic;
         }
     }
 
-        private static int Bottom()
+        static int Bottom()
         {
             List <int> b =  new List<int> ();
             {
@@ -47,9 +47,9 @@ using System.Collections.Generic;
                 return number; 
             }
         }
-        private class DividerWave
+        public class DividerWave
         {
-           private static void Wave(int n = 4, string c = "/" ,int b= 2)
+    public static void Wave(int n, string c = "/" ,int b = 3 )
             {
             
                 int Top = n;
@@ -58,21 +58,39 @@ using System.Collections.Generic;
                 float m = n/b;
         //n;Middle; Bottom;
             }
-            int n = 4;
-            int b = 2; 
-        
+//            int n = 4;
+//            int b = 2; 
         }
 
 
-        private static int Top()
+        public static int Top()
         {
             throw new NotImplementedException();
         }
 
-        public static void Main(string[]args )
+    public static void Main(string[]args )
 
     {
-        
+        static void Top()
+        {
+            Console.WriteLine("Provide a Top number between 0 & 10");
+            string Top = Console.ReadLine();
+            int n = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+
+        static void Wave()
+        {
+            Console.WriteLine("/");
+        }
+            
+        static void Bottom()
+        {
+            Console.WriteLine("Provide a bottom number between 0 & 10");
+            string bottom = Console.ReadLine();
+            int b = Convert.ToInt32(Console.ReadLine());
+        }             
 
     }
 }
