61f10ca858d5ee01bccfbbb23a62a03eb1ed481a
Godfrey Katabira <kat21010@byui.edu>
2025-03-25 12:35:07 +0200

    newly done

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -39,6 +39,16 @@ namespace Documents;
             
             return (double)_top / (double)_bottom;
     }
+
+
+    
+
+
+
+
+
+
+
     public static void Main(string[]args)
     {
 //        Console.WriteLine($"{_top1},/,{_bottom1}");
@@ -48,11 +58,31 @@ namespace Documents;
         List <int> _bottom = new List <int>();
         Console.WriteLine($"{_top1}/{_bottom1}");
 
+        int _topd = 1 ;
+        int _bottomd = 1;
+        List <double> GetDecimalValue = new List <double>();
+        
+        double div = _topd/_bottomd;
+
+        Console.WriteLine($"{ div}");
+
+
         int _top2 = 1;
         int _bottom2 = 1;
         List <int> _top3 = new List  <int>();
         List <int> _bottom3 = new List <int>();
         Console.WriteLine($"{_top2}");
+
+        int _topc = 1 ;
+        int _bottomc = 1;
+        List <double> GetDecimalValuec = new List <double>();
+        
+        double divc = _topc/_bottomc;
+
+        Console.WriteLine($"{ divc}");
+       
+
+
 
 
         int _top4 = 5;
@@ -61,12 +91,28 @@ namespace Documents;
         List <int> _bottom6 = new List <int>();
         Console.WriteLine($"{_top4}/{_bottom4}");
 
+        int _topl = 5 ;
+        int _bottoml = 1;
+        List <double> GetDecimalValuel = new List <double>();
+        
+        double div1 = _topl/_bottoml;
+
+        Console.WriteLine($"{ div1}");
+
 
         int _top5 = 5;
         int _bottom5 = 0;
         List <int> _top7 = new List <int>();
         List <int> _bottom7 = new List <int>();
         Console.WriteLine($"{_top5}");
+
+        int _topP = 5 ;
+        int _bottomP = 0;
+        List <double> GetDecimalValueP = new List <double>();
+        
+        double div2 = _topP/_bottomP;
+
+        Console.WriteLine($"{ div2}");
 
 
         
