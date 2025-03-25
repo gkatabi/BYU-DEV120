372cec99b1e3b51334d48490c731ddde4030d528
Godfrey Katabira <kat21010@byui.edu>
2025-03-25 09:30:11 +0200

    built the string fraction of it

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -41,24 +41,41 @@ namespace Documents;
     }
     public static void Main(string[]args)
     {
-        List <int> _top = new List <int>();
+//        Console.WriteLine($"{_top1},/,{_bottom1}");
         int _top1 = 1;
-        Console.Write($"{_top1}");
-        int _top2 = 1 ;
-        Console.Write($"{_top2}");
-        int _top3 = 5;
-        Console.Write($"{_top3}");
-        int _top4 = 5;
-        Console.Write($"{_top4}");
-        Console.Write($"{_top}");
-        List <int> _bottom = new List <int>();
         int _bottom1 = 1;
+        List <int> _top = new List <int>();
+        List <int> _bottom = new List <int>();
+        Console.WriteLine($"{_top1}/{_bottom1}");
+
+        int _top2 = 1;
+        int _bottom2 = 1;
+        List <int> _top3 = new List  <int>();
+        List <int> _bottom3 = new List <int>();
+        Console.WriteLine($"{_top2}");
+
+
+        int _top4 = 5;
+        int _bottom4 = 1;
+        List <int> _top6 = new List <int>();
+        List <int> _bottom6 = new List <int>();
+        Console.WriteLine($"{_top4}/{_bottom4}");
+
+
+        int _top5 = 5;
+        int _bottom5 = 0;
+        List <int> _top7 = new List <int>();
+        List <int> _bottom7 = new List <int>();
+        Console.WriteLine($"{_top5}");
+
+
+        
         Console.Write($"{_bottom1}");
-        int _bottom2 = 0;
+//        int _bottom2 = 0;
         Console.Write($"{_bottom2}");
-        int _bottom3 = 1;
+//        int _bottom3 = 1;
         Console.Write($"{_bottom3}");
-        int _bottom4 = 0;
+//        int _bottom4 = 0;
         Console.Write($"{_bottom4}");
 
     
