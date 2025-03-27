c404afb0442fa46189f945cf7dcb87a7ccfc08af
Godfrey Katabira <kat21010@byui.edu>
2025-03-27 06:20:59 +0200

    newzones

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,20 +1,154 @@
-using System;
-using System.Security.Cryptography.X509Certificates;
-namespace VideoTracker;
 
-    public class Vedeo
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+namespace VideoTracker;
+ public class Video
     {
-        public string _title = " ";    //"Big bookings", "Where Mammy wasn't", "The debt man owes God", "Learn to let go");
+        public string _title = " ";
 
         public string _author =" ";
 
         public string _length = " ";
 
-//              Space for returning
+     //   Video video = new Video(); 
+        string video1_title  = "The money man Owes God";
+        string video1_Author = "Bruchagger";
+        int video1_length    = 230;
+
+        public Video()
+        {
+
+        }
+        public void ShowvideoCritics()
+        {
+            Console.Write($"{video1_title}\n{video1_Author}\n{video1_length }");
+               
+        } 
+
+        public class Comments
+        {
+            public string _name = " "; 
+
+            public string _comments =" ";
+
+            public string _numberOfComments = " ";
+
+    //        Comment coment = new Comment(); 
+            string _name1 = "";
+            string _name2 = "" ;     
+            string _comments1 = " slow nbit nice";
+            string _comments2 = " cool movie";
+            
+            
+            
+            public Comments()
+            {
+
+            }
+            public void ShowComments()
+            {
+                Console.Write($"{_name1} {_comments1}\n {_name2} {_comments2}");
+            }
+
+
+
+        public class Count
+        {public string _allComments = " ";}
+            return _allComments.count ;
+
+        }
+
+        public Count()
+        {
+
+        }
+        public void ShowNumberOfComments()
+        {
+            Console.WriteLine("They were: "+_comments.Count());
+        }
+
+        
+    static void Main(string[] args)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public class Videos
+        {
+            
+            public string _title;
+            public string _author;
+            public string _Comments;
+            public string _Count;
+        }
+            VideosVideo  = new Video();
+
+            VideosVideo._titles = new _title
+            VideosVideo._Comments = new Comments();
+
+
+            VideosComments  = new Comments();
+
+
+
+
+
+
+
+            VideosCountz  = new Count();
+        }
+        
+        static void Main(string[] args)
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    person1.ShowWesternName();
+
+    person2 = new Person();
+    person2._givenName = "Joseph";
+    person2._familyName = "Smith";
+    person2.ShowWesternName();  
+// 
+
+
+
+
+
+      Space for returning
         public Vedeo()
         {
             
         }
+
 
 //      Since it's public lets display
         public void Show_title()
@@ -34,28 +168,28 @@ namespace VideoTracker;
     }
     public class  Comment
     {
-        public string _name = " ";
+        public string[] _name = new string[0];
 
-        public string _comment = " ";
+        public string[] _comment = new string[0];
 
-        static int _numberOfComments ;  
+        public int[] _numberOfComments = new int[0];  
 
 
         public Comment()
         {
             
         } 
-        public void Show_name()
+        public static void Show_name()
         {
             Console.Write(" ");
         }
-        public void Show_comment()
+        public static void Show_comment()
         {
             Console.Write(" ");
         }
 
 
-        public void Show_numberOfComments()
+        public static void Show_numberOfComments()
         {
             Console.Write(" ");
         }
@@ -66,11 +200,30 @@ namespace VideoTracker;
     
     static void Main(string[] args)
     {
-        Vedeo vedeo    = new Vedeo();
-         Comment comment = new Comment();
-        
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
-    }
+        string _title1          = "The money man Owes God";
+    //    string _title2          = "Where Mammy wasn't";
+    //    string _title3          = "My man Mr.Godfrey";
+    //    string _title4          = "Learn to let go";
 
+        
+        string[] _comment1     = ["I hate not having a Mom"];//"actors, pls consider fathers too","Nice to watch with my kids"];
+    //    string[] _comment3     = ["I love Irene", "He is a very patient father","I wish all rich men are a Godfrey","Would have gone for the classy Cornereah!!!"];
+    //    string[] _comment4     = ["Stupid fly burried on a cacus###", "What a pain","That was too much....for him","I need one of such heart broken men"];
+
+    //    List <string> Vedeo   = new List <string>();
+        List <string> Comment = new List<string>();
+        
+        {
+            Console.WriteLine($"Viewers  had these comments on{_title1}: ");
+            Console.WriteLine ($"{_comment1}");
+    //        Console.WriteLine($"Viewers had these comments on{_title2}: ");
+    //        Console.WriteLine ($"{_comment2}");
+    //        Console.WriteLine($"Viewers had these comments on{_title3}: ");
+    //        Console.WriteLine ($"{_comment3}");
+    //        Console.WriteLine($"Viewers had these comments on{_title4}: ");
+    //        Console.WriteLine ($"{_comment4}");
+
+        }
+    }
 }
  
