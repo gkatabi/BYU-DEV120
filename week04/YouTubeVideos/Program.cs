884ca214638b65c69258e206b344f326a45eb873
Godfrey Katabira <kat21010@byui.edu>
2025-03-27 08:47:55 +0200

    Some further

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -53,177 +53,56 @@ namespace VideoTracker;
             }
 
 
-
+        }
         public class Count
-        {public string _allComments = " ";}
-            return _allComments.count ;
-
-        }
-
-        public Count()
         {
+            public string _allComments = " ";
+            public Count()
+            {
 
-        }
-        public void ShowNumberOfComments()
-        {
-            Console.WriteLine("They were: "+_comments.Count());
-        }
-
-        
-    static void Main(string[] args)
-    {
-
-    }
+            }
+            public void ShowNumberOfComments()
+            {
+                Console.WriteLine("They were: ");
+            }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public class Videos
-        {
-            
-            public string _title;
-            public string _author;
-            public string _Comments;
-            public string _Count;
-        }
-            VideosVideo  = new Video();
-
-            VideosVideo._titles = new _title
-            VideosVideo._Comments = new Comments();
-
-
-            VideosComments  = new Comments();
-
-
-
-
-
-
-
-            VideosCountz  = new Count();
-        }
-        
         static void Main(string[] args)
 
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-    person1.ShowWesternName();
-
-    person2 = new Person();
-    person2._givenName = "Joseph";
-    person2._familyName = "Smith";
-    person2.ShowWesternName();  
-// 
-
-
-
-
-
-      Space for returning
-        public Vedeo()
-        {
-            
-        }
-
-
-//      Since it's public lets display
-        public void Show_title()
-        {
-            Console.Write($"{_title}");
-
-        }
-        public void Show_author()
-        {
-            Console.Write($"{_author}");
-        }
-        public void Show_length()
-        {
-            Console.Write($"{_length}");
-        }
-
-    }
-    public class  Comment
-    {
-        public string[] _name = new string[0];
-
-        public string[] _comment = new string[0];
-
-        public int[] _numberOfComments = new int[0];  
-
-
-        public Comment()
-        {
-            
-        } 
-        public static void Show_name()
-        {
-            Console.Write(" ");
-        }
-        public static void Show_comment()
-        {
-            Console.Write(" ");
-        }
-
-
-        public static void Show_numberOfComments()
-        {
-            Console.Write(" ");
-        }
-
-
-    
-    
-    
-    static void Main(string[] args)
-    {
-        string _title1          = "The money man Owes God";
-    //    string _title2          = "Where Mammy wasn't";
-    //    string _title3          = "My man Mr.Godfrey";
-    //    string _title4          = "Learn to let go";
-
-        
-        string[] _comment1     = ["I hate not having a Mom"];//"actors, pls consider fathers too","Nice to watch with my kids"];
-    //    string[] _comment3     = ["I love Irene", "He is a very patient father","I wish all rich men are a Godfrey","Would have gone for the classy Cornereah!!!"];
-    //    string[] _comment4     = ["Stupid fly burried on a cacus###", "What a pain","That was too much....for him","I need one of such heart broken men"];
-
-    //    List <string> Vedeo   = new List <string>();
-        List <string> Comment = new List<string>();
-        
-        {
-            Console.WriteLine($"Viewers  had these comments on{_title1}: ");
-            Console.WriteLine ($"{_comment1}");
     //        Console.WriteLine($"Viewers had these comments on{_title2}: ");
     //        Console.WriteLine ($"{_comment2}");
     //        Console.WriteLine($"Viewers had these comments on{_title3}: ");
     //        Console.WriteLine ($"{_comment3}");
     //        Console.WriteLine($"Viewers had these comments on{_title4}: ");
     //        Console.WriteLine ($"{_comment4}");
+    //"actors, pls consider fathers too","Nice to watch with my kids"];
+    //    string[] _comment3     = ["I love Irene", "He is a very patient father","I wish all rich men are a Godfrey","Would have gone for the classy Cornereah!!!"];
+    //    string[] _comment4     = ["Stupid fly burried on a cacus###", "What a pain","That was too much....for him","I need one of such heart broken men"];
+
+        {
+            Video video = new Video();
+        
+            Video _title = new Video();
+            string _title1  = "Why Cub drivers park near the bush";
+            string _author1 = "Long Foot";
+            int _length1 = 2344;
+            Console.WriteLine($"On your video\n{_title1}\nBy {_author1}\nOf length {_length1} Seconds");   
+            
+           Comments _comments = new Comments();
+           string _name = "Gerald";
+           string _comment1   = "Why is it so long?";  
+           Console.WriteLine($"{_name}Commented:\n{_comment1}");
+
+
+           Video myVideo =  new Video();
+           myVideo._title = "True Roads to Africa";
+           
+           
+           
+           
 
         }
+        
     }
 }
  
