5271d3b3d08aaf89421a3ae31a52a4a1d77473db
Godfrey Katabira <kat21010@byui.edu>
2025-04-02 11:19:37 +0100

    rebuilt  with different files

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,65 +1,137 @@
 using System;
-using System.ComponentModel;
-namespace Programs;
+//using System.ComponentModel;
+//using static Programs.Activity;
+namespace Programs
+{
 
-    class Activity
+    public class Activity
     {
-        public string _activityDescription;
-        protected int _activityDuration = 3000 ;
-        private string _activityBreath ;
-        private string _activityEndMessage;
-        private string description;
-        private string breath;
-
-        public Activity(string breath)
+        private string _description= "Well  come to the mindfull activity";
+        private string _seconds = "";
+        private string _prompts ="";
+        private string _endMessage ="";
+        public string GetDescription()
         {
-            this.breath = breath;
+            return _description;
+
         }
+        public void SetDescription(string description )
+        {
+            _description = description;
+        }
+
+//      value for time delays chosen
+        public string GetSeconds()
+        {
+            return _seconds;
+
+        }
+        public void SetSeconds(string seconds)
+        {
+        _seconds = seconds;
+        }
+
+// Get and set prompts
+
+        public string GetPrompts()
+        {
+            return _prompts;
+
+        }
+        public void SetPrompts(string prompts)
+        {
+        _prompts = prompts;
+        }
+
+//   Set up eneMessages
+
+        public string GetEndMessage()
+        {
+            return _endMessage;
+
+        }
+        public void SetEndMessage(string endMessage)
+        {
+        _endMessage = endMessage;
+        }
+    }
+}
+
+//        public string _activityDescription;
+//        protected int _activityDuration = 3000 ;
+//        private string _activityBreath;
+//        private string _activityEndMessage;
+  
+//        private string description;
+//        private string breathe;
+
+//        public Activity(string breathe)
+//        {
+//            this.breathe = breathe;
+//        }
 
     //    private int duration;
 
 
-        public class Breath:Activity
-    {
-        protected string _breath;
-        public void BreathOut(string breath)
-        {
-        }
-            public Breath(string breath) : base(breath)
-            {
-                _breath  = breath;
-            } 
+//    public class Breath:Activity
+//    {
+//        protected string _breathe;
+//        public void BreathOut(string breathe)
+//        {
+//        }
+//        public Breath(string breathe) : base(breathe)
+//        {
+//            _breathe  = breathe;
+//        }
+
+//        public  string Breathe()
+//        {
         
-    
+//            return _breathe;
+//        }
+
+//        public string Get_description()
+//        {
+//        _activityDescription = description;
+//        return _activityDescription;
+
+//    }
+//    public void Set_activityDescription(string description )
+  
+//    {
+//        _activityDescription = description;
+//    }
+//    public  void  GetActivityEndMessage()
+//    {
+//        _activityEndMessage = "OK";
+//        Console.WriteLine("Tell me so");
+
+//    }
+
+//        internal static string GetBreath()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public class Reflection:Activity
+//    {
+//        public string GetRandomPrompt;
+        
+//        public Reflection(string breathe = "breathe In",string randomPrompt="Think of a time when you stood up for someone else.") : base(breathe= "breathe in")
+//        { 
+//            string _randomPrompt  = randomPrompt;
+//            Console.WriteLine($"{_randomPrompt}");
+
+//        }
 
 
-
-
-
-
-        public string Get_description()
-        {
-        _activityDescription = description;
-        return _activityDescription;
-
-    }
-    public void Set_activityDescription(string description )
-    {
-        _activityDescription = description;
-    
-
-    }
-    public  void  GetActivityEndMessage()
-    {
-        _activityEndMessage = "OK";
-        Console.WriteLine("Tell me so");
-
-    }
-
-
-    }
-
-    public class duration
-    {
-    }
-}
+//    public class Listing : Activity
+//    {
+//        public Listing(string breathe = "Breathe out!", string items = "",int itemsCount=5)   : base(breathe = "Breathe out!")
+//        {
+//            Console.WriteLine("List your items for me please");
+//        }
+//        }
+//    }
+//}
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,45 +1,103 @@
 using System;
-using System.ComponentModel;
-namespace Activities;
-
+//using System.ComponentModel;
+//using static Programs.Activity;
+namespace Documents
+{
     class Activity
     {
-        private string _description;
-        public int _duration;
-        private string _breath ="In";
-        public string _endMessage;
-    public class breathOut:Activity
+        public Activity SetPrompts { get; private set; }
+
+        public static void Main(string[]args)
     {
-        private string _breath1 = "Out";
-    }
-
-    public class Reflection:Activity
-    {
-        private string description1;
-        public int _duration1;
-        private string _randomPrompt;
-        private string _reflect;
-        public string  _endMessage1;
-
-    }
-
-    public class Listing:Activity
-    {
-        private string _description2;
-        private int _duration2;
-        private string _radomPrompt2;
-        public string _items;
-        public int _itemsCount;
-        public int _count;
-    }
-    static void Main (string[]args)
-
-    {
-        Activity breath = new Activity();
-        Activity reflection = new Activity();
-        Activity Listing = new Activity();
-
-        Console.WriteLine("You've accomplished the activities");
-    }    
+        Activity activity = new Activity();
         
+
+    
+        activity.SetDescription("breathe in !");
+        activity.SetSeconds("5");
+
+        activity.SetPrompts = new Activity();
+//        activity.SetEndMessage();
+//        {
+//        ListingActivity = new ();
+//        activityListing.SetListedItems();
+//        activityListing.SetTotalItems();
+
+        }
+
+        public void SetEndMessage(string EndMessage)
+        {
+            Console.WriteLine("You will improve the more you re-do so");
+        }
+
+        public void SetSeconds(string _seconds)
+        {
+            Console.WriteLine("How many seconds will it take you to think response over in this activity?");
+            string Seconds = Console.ReadLine();
+            int seconds = Convert.ToInt32($"{Seconds}");
+        }
+
+        public void SetDescription(string _description)
+        {
+            Console.Write("Welcome to the mindfull activities.They will be 3 only of which will relax your min.\nDo you want to continue? ");
+            string description = Console.ReadLine();
+            
+            
+            
+        }
+
+        public string GetActivityInfo()
+        {
+            return $"{GetActivityInfo}";
+            Console.WriteLine("You are in");
+        }
+    }
 }
+
+//        private string _description;
+//        public int _duration;
+//        private string _breathe;
+//        public string _endMessage;
+//    public class breatheOut:Activity
+//    {
+//        private string _breath1 ;
+//    }
+
+//    public class Reflection:Activity
+//    {
+//        private string description1;
+//        public int _duration1;
+//        private string _randomPrompt;
+//        private string _reflect;
+//        public string  _endMessage1;
+
+//    }
+
+//    public class Listing:Activity
+//    {
+//        private string _description2;
+//        private int _duration2;
+//        private string _radomPrompt2;
+//        public string _items;
+//        public int _itemsCount;
+//        public int _count;
+//    }
+//    static void Main (string[]args)
+
+//    {
+//        Activity activity = new Activity();
+//        Breathe breathe = new Breathe();
+
+//        Reflection reflection = new Reflection();
+//        Listing listing = new Listing();
+
+//        Console.WriteLine("You've accomplished the activities");
+//    }    
+        
+//}
+
+//internal class Breathe
+//{
+//    public Breathe()
+//    {
+//    }
