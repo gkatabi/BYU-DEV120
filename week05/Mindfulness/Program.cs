b5ea5cbfb56c1a46f845f1095fc4da97a6cf70ae
Godfrey Katabira <kat21010@byui.edu>
2025-04-03 06:56:50 +0100

    started afresh

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,55 +1,78 @@
 using System;
 namespace Mindfulness;
 
-    public class Breathing
+    public class Activity
     {
-        private int _breatheDuration;
-        
-        private string _breathIn;
-        private int _timer;
-        private string _breatheOut;
-        public int GetDuration()
-        {
-            Console.WriteLine("How many seconds in numeral do you wish to take thinking of your response");
-            string seconds = Console.ReadLine();
-            int Seconds = Convert.ToInt32($"{seconds}");
-            return  Seconds;
-        }
+       protected int _duration;
+       protected string _description = "";
+       protected int _timePause;
+       protected string _endMessage = "";
 
-        
-    } 
-    public class Reflect
+       public Activity()
+       {
+
+       }
+       public int  GetDuration()
+       {
+            return _duration;
+       }
+       public void SetDuration(int duration)
+       {
+           _duration = duration;
+       }
+
+       public string  GetDescription()
+       {
+            return _description;
+       }
+       public void SetDescription(string description)
+       {
+           _description = description;
+       }
+
+       public int  GetTimePause()
+       {
+            return _timePause;
+       }
+       public void SetTimePause(int timePause)
+       {
+           _timePause = timePause;
+       }
+
+       public string  GetEndMessage()
+       {
+            return _endMessage;
+       }
+       public void SetEndMessage(string endMessage)
+       {
+           _endMessage = endMessage;
+       }
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void Main(string[] args)
     {
-        private int _reflectDuration; // Get duration from user
-        private string _description;
-        private static string _randomThoughtPrompt;
-        // Should display a prompt question related to the experience 
-        // Why?, What?, How?
-        private static string _randomExperience; 
-// The methods which show and display sould be used here because there will be need of showing   
-        public int GetDuration()
-        {
-            return Seconds;
-        }     
-
-    public class ListingActivity
-    {
-        private string _duration;
-        private string _description;
-        private String _randomPrompt;
-// Show styling should be used here because there will be need of displaying 
-//After displaying the prompt, the program should give them a countdown of several seconds to
-//  begin thinking about the prompt. Then, it should prompt them to keep listing items. 
-
-        private string _items;
-//The user lists as many items as they can until they they reach the duration specified by the user at the beginning.
-//The activity them displays back the number of items that were entered.
-//The activity should conclude with the standard finishing message for all activities.
-
-
-    }
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Welcome to this Mindful program where you will re discover relaxation and strength to carry on with a relaxed mind.");
+        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
     }
 }
