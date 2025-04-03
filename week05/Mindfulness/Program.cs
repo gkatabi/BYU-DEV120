266caf79bfb8966359b219e7682dbea8fe5e75e7
Godfrey Katabira <kat21010@byui.edu>
2025-04-03 07:36:30 +0100

    Started afresh

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,74 +1,8 @@
 using System;
 namespace Mindfulness;
 
-    public class Activity
+    class Program
     {
-       protected int _duration;
-       protected string _description = "";
-       protected int _timePause;
-       protected string _endMessage = "";
-
-       public Activity()
-       {
-
-       }
-       public int  GetDuration()
-       {
-            return _duration;
-       }
-       public void SetDuration(int duration)
-       {
-           _duration = duration;
-       }
-
-       public string  GetDescription()
-       {
-            return _description;
-       }
-       public void SetDescription(string description)
-       {
-           _description = description;
-       }
-
-       public int  GetTimePause()
-       {
-            return _timePause;
-       }
-       public void SetTimePause(int timePause)
-       {
-           _timePause = timePause;
-       }
-
-       public string  GetEndMessage()
-       {
-            return _endMessage;
-       }
-       public void SetEndMessage(string endMessage)
-       {
-           _endMessage = endMessage;
-       }
-
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     public static void Main(string[] args)
