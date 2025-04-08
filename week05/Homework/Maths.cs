c00f9bf40845748d46a7f4d32a7b28b77693e202
Godfrey Katabira <kat21010@byui.edu>
2025-04-08 08:52:11 +0200

    All of

---
+++ week05/Homework/Maths.cs
@@ -0,0 +1,23 @@
+using System;
+namespace Homework
+
+{
+    class Assignment
+    {
+        public string _assignmdnt;
+        public string _lesson;
+    }
+    public class Math
+    {
+
+    
+        public string _topic = "graphs";
+    
+    Assignment assignment = new Assignment();
+    Math Assignment ;
+    
+//    public class Math: Assignment()
+    
+        string _studentName = "Godfrey"; 
+        }
+}
